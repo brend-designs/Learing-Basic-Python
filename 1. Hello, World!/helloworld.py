1bf93e7129093e1058026d1198fbb625c32488c9
Brendan Langenhoff <me@brend.design>
2021-05-29 19:06:44 +1000

    1. Hello World

---
+++ 1. Hello, World!/helloworld.py
@@ -0,0 +1,12 @@
+# Project Name: Hello World
+# Version: 1.0
+# Author: Brendan Langenhoff (brend-designs)
+# Description: Simply prints "Hello World" to command line (console)
+
+# 1. Print to console
+print("Hello World")
+
+
+# 2. Getting input from console
+print("Please enter your name:")
+print(f"Hello there, {str(input())}!")
