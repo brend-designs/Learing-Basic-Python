5848c3dbdfe216363862e9bd53c7388d88eefb86
Brendan Langenhoff <me@brend.design>
2021-05-29 20:33:11 +1000

    2. Variables and Types

---
+++ 2. Variables & Types/variablestypes.py
@@ -0,0 +1,51 @@
+# Project Name: Variables and Types
+# Version: 1.0
+# Author: Brendan Langenhoff (brend-designs)
+# Description: Experimenting with variables and different data types.
+
+# 1. Define an Integer (int) - Whole number
+# ================================================================
+my_int = 7
+print(f"Integer: {my_int}\n")
+# ================================================================
+
+
+# 2. Define a Float (float) - Floating point number (decimal)
+# ================================================================
+my_float = 7.0
+print(f"Float (v1): {my_float}\n")
+my_float = float(7)
+print(f"Float (v2): {my_float}\n")
+# ================================================================
+
+
+# 3. Define a String (str)
+# ================================================================
+my_string = 'Hello there!'  # Single Quotes
+print(f"String (Single Quotes): {my_float}\n")
+
+# Double Quotes (Makes it easier to use apostrophes)
+my_string = "Don't forgot to say Hello!"
+print(f"String (Double Quotes): {my_float}\n")
+# ================================================================
+
+
+# 4. Simple Operators
+# ================================================================
+x = 1
+y = 2
+# Add 2 Integers together
+print(f"Simple Operator (Integers): {x+y}\n")
+
+hello = "Hello"
+world = "World"
+# Add 2 Strings together (String Concatenation)
+print(f"Simple Operator (Strings): {hello} {world}\n")
+# ================================================================
+
+
+# 5. Defining variables "simultaneously" on the same line
+# ================================================================
+a, b = 3, 4
+print(f"Defining variables simultaneously: {a, b}")
+# ================================================================
